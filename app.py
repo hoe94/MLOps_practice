9fc31c3378aa890acbbff9594e5fa0da9bf8a89d
Hoe <lkh_bear94@hotmail.com>
2021-05-19 14:44:14 +0800

    update app

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ def index():
         except Exception as e:
             print(e)
             #error = {"error": "Something went wrong!! Try agin"}
-            return render_template("404.html", error = e)
+            error = {"error": e}
+            return render_template("404.html", error = error)
     else:
         return render_template("index.html")
 
