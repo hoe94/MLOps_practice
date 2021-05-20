1110ea8ed2c7ee4ae8c0a260a3d640b808963feb
Hoe <lkh_bear94@hotmail.com>
2021-05-20 09:46:05 +0800

    test done

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,7 @@ import logging
 import os
 import joblib
 import pytest
-from prediction_service.prediction import form_response, api_response
+from prediction_service.prediction import NotInRange, form_response, api_response
 import prediction_service
 
 input_data = {
@@ -63,3 +63,14 @@ def test_api_response_correct_range(data = input_data["correct_range"]):
     res = api_response(data)
     assert TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
 
+def test_form_response_incorrect_range(data = input_data["incorrect_range"]):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        res = form_response(data)
+
+def test_api_response_incorrect_range(data = input_data["incorrect_range"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInRange().message
+
+#def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
+#    res = api_response(data)
+#    assert res["response"] == prediction_service.prediction.NotInCols().message
